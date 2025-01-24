7312c66666ebdcd7248095e01dc11d2199c42312
uchenna <uenyita@my.centennialcollege.ca>
2025-01-24 14:00:03 -0500

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,105 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(PayCalculatorApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class PayCalculatorApp extends StatelessWidget {
+  const PayCalculatorApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Pay Calculator',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: PayCalculatorScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class PayCalculatorScreen extends StatefulWidget {
+  const PayCalculatorScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _PayCalculatorScreenState createState() => _PayCalculatorScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _PayCalculatorScreenState extends State<PayCalculatorScreen> {
+  final TextEditingController _hoursController = TextEditingController();
+  final TextEditingController _rateController = TextEditingController();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  double regularPay = 0.0;
+  double overtimePay = 0.0;
+  double totalPay = 0.0;
+  double tax = 0.0;
+
+  void calculatePay() {
+    final double hoursWorked = double.tryParse(_hoursController.text) ?? 0.0;
+    final double hourlyRate = double.tryParse(_rateController.text) ?? 0.0;
+
+    if (hoursWorked <= 40) {
+      regularPay = hoursWorked * hourlyRate;
+      overtimePay = 0.0;
+    } else {
+      regularPay = 40 * hourlyRate;
+      overtimePay = (hoursWorked - 40) * hourlyRate * 1.5;
+    }
+
+    totalPay = regularPay + overtimePay;
+    tax = totalPay * 0.18;
+
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      appBar: AppBar(title: Text('Pay Calculator')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            // Input Fields
+            TextField(
+              controller: _hoursController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(labelText: 'Number of Hours Worked'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            TextField(
+              controller: _rateController,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(labelText: 'Hourly Rate'),
+            ),
+            SizedBox(height: 16),
+
+            // Calculate Button
+            ElevatedButton(
+              onPressed: calculatePay,
+              child: Text('Calculate'),
+            ),
+
+            SizedBox(height: 16),
+
+            // Output Data
+            Text('Regular Pay: \$${regularPay.toStringAsFixed(2)}'),
+            Text('Overtime Pay: \$${overtimePay.toStringAsFixed(2)}'),
+            Text('Total Pay (before tax): \$${totalPay.toStringAsFixed(2)}'),
+            Text('Tax: \$${tax.toStringAsFixed(2)}'),
+
+            Spacer(),
+
+            // About Me Section
+            Center(
+              child: Text(
+                'Name: Uchenna Enyita\nStudent ID: 301258888,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
